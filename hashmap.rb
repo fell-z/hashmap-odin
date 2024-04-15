9c7e8a0aeadac8e972aa6e7d7d8e519a3b628aa0
fell-z <matheusfelipeagapito@hotmail.com>
2024-04-15 17:16:02 -0300

    Implement a method to update the hashmap
    - Added a method to set a key-value pair into the hashmap
    - Added a var to hold the key-value pairs and set to an initial size
    - Added a method to prevent indexing the hashmap out of its bounds

--- hashmap.rb
+++ hashmap.rb
@@ -1,5 +1,17 @@
 # Implementation of a hashmap
 class HashMap
+  def initialize
+    @buckets = Array.new(16) { nil }
+  end
+
+  def set(key, value)
+    index = index_from_key(key)
+
+    verify_index(index)
+
+    @buckets[index] = [key, value]
+  end
+
   def hash(key)
     hash_code = 0
     prime_number = 31
@@ -7,5 +19,19 @@ class HashMap
     key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
 
     hash_code
+  end
+
+  def capacity
+    @buckets.size
+  end
+
+  private
+
+  def index_from_key(key)
+    hash(key) % capacity
+  end
+
+  def verify_index(index)
+    raise IndexError if index.negative? || index >= capacity
   end
 end
